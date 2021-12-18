04b7d4d399e49b430a0950a797986fa4feffd28c
Akash Tiwari <77334919+akashtiwari2003@users.noreply.github.com>
2021-12-18 17:03:37 +0530

    Update miniProject.cpp

--- miniProject.cpp
+++ miniProject.cpp
@@ -1,55 +1,125 @@
 #include <bits/stdc++.h>
 #include <windows.h>
-#define loop(i,j) for(int i = 0; i<j; ++i)
+#define loop(i, j) for (int i = 0; i < j; ++i)
 using namespace std;
 
-void printSpaces(int n){
-      loop(i,n)
-        cout << " ";
+vector<int> getArrayInput(){
+    int n,i;
+    cout << "Enter the number of elements:- ";
+    cin >> n;
+    vector<int> v(n);
+    for(i = 0; i<n; ++i){
+        cout << "Element " << i+1 << ":- ";
+        cin >> v[i];
+    }
+    return v;
 }
 
-void printInBox(int n){
-    int cnt = floor(log10(n)+1);
-
+void printSpaces(int n)
+{
+    loop(i, n)
+        cout<< " ";
 }
 
-void printBorder(vector<int> v,int index,string box){
-    loop(i,v.size()){
-        if(i == index){
+void printBorder(vector<int> v, int index, string box)
+{
+    loop(i, v.size())
+    {
+        if (i == index)
+        {
             cout << box << endl;
             break;
         }
-        printSpaces(floor(log10(v[i])+1)+3);
+        printSpaces(floor(log10(v[i]) + 1) + 3);
     }
 }
 
-void printArray(vector<int> v, int index){
+void printArray(vector<int> v, int index)
+{
     string box = "+";
-    int cntdigits = floor(log10(v[index])+1);
-    loop(i,cntdigits+2){
+    int cntdigits = floor(log10(v[index]) + 1);
+    loop(i, cntdigits + 2)
+    {
         box += "-";
     }
-    box+="+";
-    printBorder(v,index,box);
-    loop(i,v.size()){
-        if(i == index){
-            
+    box += "+";
+    printBorder(v, index, box);
+    loop(i, v.size())
+    {
+        if (i == index)
+        {
+
             cout << "| " << v[i] << " |";
         }
-        else{
+        else
+        {
             cout << v[i];
         }
         printSpaces(3);
     }
     cout << endl;
-    printBorder(v,index,box);
+    printBorder(v, index, box);
 }
 
+void printMainMenu()
+{
+    cout << "1 -> Searching\n2 -> Sorting\n3 -> Exit\nEnter your choice :- ";
+}
 
-int main(){
-  vector<int> v;
-  string name;
-  cout << "Please Enter Your Name:- ";
-  getline(cin,name);
-  return 0;
+void linearSearch(){
+    int i,n,tmp,t,notFound = 0;
+    string ip;
+    vector<int> v = getArrayInput();
+    cout << "Enter the element to search :- ";
+    cin >> tmp;
+    cout << "Enter the time delay in seconds:- ";
+    cin >> t;
+    for(i = 0; i<v.size(); ++i){
+        Sleep(t*1000);
+        printArray(v,i);
+        if(v[i] == tmp){
+            cout << tmp << " is at index "<< i << endl;
+            ++notFound;
+            break;
+        }
+    }
+    if(!notFound){
+        cout << tmp << "not found in the given list!!" << endl;
+    }
+    system("PAUSE");
+}
+int main()
+{
+    vector<int> v;
+    int choice, choice2;
+    string name;
+    cout << "Please Enter Your Name :- ";
+    getline(cin, name);
+    cout << "\nWelcome to S&S Simplified!!! " << name << "\n";
+        
+    while(1){
+        printMainMenu();
+        cin >> choice;
+        if(choice == 1){
+        cout << "1 -> Linear Search\n2 -> Binary Search\n";
+        cout << "Select one to go ahead :- ";
+        cin >> choice2;
+        if(choice2 == 1){
+            linearSearch();
+        }
+        }
+        else if(choice == 2){
+            cout << "1 -> Bubble Sort\n2 -> Insertion Sort\n3 -> Merge sort\n4 -> Quick Sort\n";
+            cout << "Select one to go ahead :- ";
+            cin >> choice2;
+        }
+        else if(choice == 3){
+            exit(0);
+        }
+        else
+            cout << "Invalid choice!! Try Again\n";
+    }
+
+    
+    return 0;
 }
